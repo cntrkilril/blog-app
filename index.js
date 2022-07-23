03027e2602fa995073ecdc9d4b0fc67dac568d2e
cntrkilril <shalkir2002@mail.ru>
2022-07-23 15:39:08 +0300

    deploy

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const __filename = fileURLToPath(import.meta.url);
 
 export const __dirname = path.dirname(__filename);
 
+console.log(__dirname)
+
 app.get('/',(req, res) => {
     res.sendFile( path.resolve(__dirname, './public/index.html'))
 })
